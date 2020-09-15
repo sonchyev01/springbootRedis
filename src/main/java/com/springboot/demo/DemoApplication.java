e5d37134730d821ca20beb2096f31984e747b82c
sonchyev01 <59448473+sonchyev01@users.noreply.github.com>
2020-09-16 00:28:06 +0800

    Update DemoApplication.java

--- src/main/java/com/springboot/demo/DemoApplication.java
+++ src/main/java/com/springboot/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+ygg
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer {
 
